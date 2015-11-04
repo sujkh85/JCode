69363ccd840c8b6fb4f32a963cb116d097c4a810
sujkh85 <sujkh@nomadconnection.com>
2015-11-04 20:21:07 +0900

    logger.js add

---
+++ javascript/modules/logger.js
@@ -0,0 +1,54 @@
+//level : info warn debug error log
+
+/*
+*  param :
+*    level : string
+*    message : string
+*/
+function logger(level, message){
+  //필요변수 선언
+  var info = {
+    pre : '%c [INFO] ',
+    color : 'color: green'
+  },
+  warn = {
+    pre : '%c [WARN] ',
+    color : 'color: red'
+  },
+  debug = {
+    pre : '%c [DEBUG] ',
+    color : 'color: bule'
+  },
+  error = {
+    pre : '%c [ERROR] ',
+    color : 'color: purple'
+  },
+  log = {
+    pre : '%c [LOG] ',
+    color : 'color: black'
+  }
+
+  //유효성 검사
+  typeof level === 'string' && typeof message === 'string' ? true : !function(){
+    console.log(error.pre + 'logger type error' , error.color);
+  }();
+
+  //case
+  switch(level){
+    case 'info' : console.log(info.pre + message , info.color);
+      break;
+    case 'warn' : console.log(warn.pre + message , warn.color);
+      break;
+    case 'debug' : console.log(debug.pre + message , debug.color);
+      break;
+    case 'error' : console.log(error.pre + message , error.color);
+      break;
+    case 'log' : console.log(log.pre + message , log.color);
+      break;
+    default : console.log(message);
+  }
+
+}
+
+logger(1 , 'test입니다.');
+//logger('info' , 'test입니다.');
